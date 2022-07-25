a5714d0e4851e5128d3f195f15ac7cd31955e9e1
TheLonerCoder <thecodingloner@gmail.com>
2022-07-24 19:04:17 -0500

    homeprojects update
    layout and style changes, flexbox

--- loner-coder/src/components/homeProjects.js
+++ loner-coder/src/components/homeProjects.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../styles/home.css';
 
 function HomeProjects() {
   return (
-    <div>
+    <div className='homeProjects'>
         <h2>Project Highlights</h2>
-        <section>
-            <a href=""><img src="" alt="" className='placeholders'/></a>
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
+        <section id='imageBox'>
+            <a href=""><img src="imgs/Forge-the-Fort.jpg" alt="" className='placeholders'/></a>
+            <a href=""><img src="imgs/VR-WORLD.jpg" alt="" /></a>
+            <a href=""><img src="imgs/solo.jpg" alt="" /></a>
+            <a href=""><img src="imgs/worlda-green.jpg" alt="" /></a>
         </section>
 
         <button>Full Porfolio</button>
