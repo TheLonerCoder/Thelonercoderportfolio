a4d2914ef5e2db60a99b7c809e2d5b7b5b1af2e7
TheLonerCoder <thecodingloner@gmail.com>
2022-07-24 21:41:32 -0500

    style update

--- loner-coder/src/components/journey.js
+++ loner-coder/src/components/journey.js
@@ -15,73 +15,65 @@ function Journey() {
 
         <section id='journey1'>
           <h3>Technical Competencies</h3>
-          <p>
+          <p>Represention of the usage/comfortability level of these languages, frameworks, and libraries in my projects</p>
           
           <ul>
             <li>
               <h4>React</h4>
-              <div className="bar"><div className='success'></div></div>
+              <div className="bar"><div className='reactBar'></div></div>
             </li>
 
             <li>
               <h4>Javascript</h4>
-              <div className="bar"><div className='success'></div></div>
+              <div className="bar"><div className='javascriptBar'></div></div>
             </li>
 
             <li>
               <h4>HTML</h4>
-              <div className="bar"><div className='success'></div></div>
+              <div className="bar"><div className='htmlBar'></div></div>
             </li>
 
             <li>
               <h4>CSS</h4>
-              <div className="bar"><div className='success'></div></div>
+              <div className="bar"><div className='cssBar'></div></div>
             </li>
 
             <li>
               <h4>SASS/SCSS</h4>
-              <div className="bar"><div className='success'></div></div>
-            </li>
-
-            <li>
-              <h4>Three.js</h4>
-              <div className="bar"><div className='success'></div></div>
-            </li>
-
-            <li>
-              <h4>Node.js</h4>
-              <div className="bar"><div className='success'></div></div>
-            </li>
-
-            <li>
-              <h4>Express.js</h4>
-              <div className="bar"><div className='success'></div></div>
+              <div className="bar"><div className='sassBar'></div></div>
             </li>
 
             <li>
               <h4>Python</h4>
-              <div className="bar"><div className='success'></div></div>
+              <div className="bar"><div className='pythonBar'></div></div>
             </li>
 
             <li>
-              <h4>MongoDB</h4>
-              <div className="bar"><div className='success'></div></div>
+              <h4>Three.js</h4>
+              <div className="bar"><div className='threeBar'></div></div>
+            </li>
 
+            <li>
+              <h4>Node.js</h4>
+              <div className="bar"><div className='nodeBar'></div></div>
+            </li>
+
+            <li>
+              <h4>Express.js</h4>
+              <div className="bar"><div className='expressBar'></div></div>
+            </li>
+
+
+            <li>
+              <h4>MongoDB</h4>
+              <div className="bar"><div className='mongoBar'></div></div>
             </li>
 
           </ul>
           
+      </section>
 
 
-
-
-
-
-
-
-</p>
-          picture
-        </section>
           
         <section id='journey2'>
             <h3>My Web Development Journey</h3>
